5a7dd0859c85fd4a10ba66647256de97ef7083d7
Rodrigo <rodrigimix@protonmail.com>
2022-05-01 12:50:03 +0100

    Hallelluia

--- EDA/T1/main.c
+++ EDA/T1/main.c
@@ -7,7 +7,7 @@ int main()
 {
     printf("Hello world\n");
     BigInt a = big_new("-101010101");
-    BigInt b = big_new("10203050000");
+    BigInt b = big_new("-10203050000");
     print_b(a);
     print_b(b);
     return 0;
