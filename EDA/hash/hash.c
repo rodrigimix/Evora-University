319aacb85ec1942fb0cc6d8172aed082f5fb2e1a
Rodrigo <rodrigimix@protonmail.com>
2022-06-05 11:55:49 +0100

    EDA: hash prime done

---
+++ EDA/hash/hash.c
@@ -0,0 +1,156 @@
+#include "hash-linear.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define Error(Str) FatalError(Str)
+#define FatalError(Str) fprintf(stderr, "%s\n", Str), exit(1)
+
+#define MinTableSize (10)
+
+enum KindOfEntry
+{
+    Legitimate,
+    Empty,
+    Deleted
+};
+
+struct HashEntry
+{
+    ElementType Element;
+    enum KindOfEntry Info;
+};
+
+typedef struct HashEntry Cell;
+
+/* Cell *TheCells will be an array of */
+/* HashEntry cells, allocated later */
+struct HashTbl
+{
+    int Ocupados;
+    int TableSize;
+    Cell *TheCells;
+};
+
+/* Return next prime; assume N >= 10 */
+
+static int
+NextPrime(int n)
+{
+    int m = n / 2, flag = 0;
+    for (int i = 2; i <= m; i++)
+    {
+        if (n % i == 0)
+        {
+            return NextPrime(n+1);
+            flag = 1;
+        }
+    }
+
+    if (flag == 0)
+    {
+        return n;
+    }
+}
+
+/* Hash function for integers */
+Index Hash(ElementType Key, int TableSize)
+{
+    return Key % TableSize;
+}
+
+LinHashTable InitializeTable(int TableSize)
+{
+    LinHashTable H;
+    int i;
+
+    if (TableSize < MinTableSize)
+    {
+        Error("Table size too small");
+        return NULL;
+    }
+
+    /* Allocate table */
+    H = malloc(sizeof(struct HashTbl));
+    if (H == NULL)
+        FatalError("Out of space!!!");
+
+    // H->TableSize = NextPrime( TableSize );
+    // Para que possa ver os resultados dos exercícios
+
+    H->TableSize = NextPrime(TableSize);
+    /* Allocate array of Cells */
+    H->TheCells = malloc(sizeof(Cell) * H->TableSize);
+    if (H->TheCells == NULL)
+        FatalError("Out of space!!!");
+
+    for (i = 0; i < H->TableSize; i++)
+        H->TheCells[i].Info = Empty;
+
+    return H;
+}
+/* END */
+
+Position ProcPos(ElementType Key, LinHashTable H)
+{
+}
+/* END */
+
+bool Find(ElementType Key, LinHashTable H)
+{
+    return false;
+}
+
+void Insert(ElementType Key, LinHashTable H)
+{
+}
+/* END */
+
+float LoadFactor(LinHashTable H)
+{
+}
+LinHashTable Rehash(LinHashTable H)
+{
+    int i, OldSize;
+    Cell *OldCells;
+
+    OldCells = H->TheCells;
+    OldSize = H->TableSize;
+
+    /* Get a new, empty table */
+    H = InitializeTable(2 * OldSize);
+
+    /* Scan through old table, reinserting into new */
+    for (i = 0; i < OldSize; i++)
+        if (OldCells[i].Info == Legitimate)
+            Insert(OldCells[i].Element, H);
+
+    free(OldCells);
+
+    return H;
+}
+/* END */
+
+ElementType Retrieve(Position P, LinHashTable H)
+{
+    return H->TheCells[P].Element;
+}
+
+void DestroyTable(LinHashTable H)
+{
+    free(H->TheCells);
+    free(H);
+}
+
+void PrintTable(LinHashTable H)
+{
+}
+
+int main(){
+    int n;
+    printf("Digite numero: ");
+    scanf("%d", &n);
+    n = NextPrime(n);
+    printf("%d\n", n);
+}
